2930c1d88dd8259b5ecc16d5a557ade34dd3976e
Sabin Hamal <highsabin987@gmail.com>
2024-05-11 08:36:39 +0545

    .

---
+++ Stack and Heap Memory.js
@@ -0,0 +1,27 @@
+//Stack Memory is only for primitive data types
+//Basically in stack memory the copy of the variable is taken
+
+let a = "Hello World";
+let b = a;
+a = "Bye World";
+
+console.log(a); //Bye World
+console.log(b); //Hello World (b gets the copy of a);
+
+
+
+//Heap Memory is for non primitive data types
+//In Heap Memory the reference of original veriable is taken
+
+let obj1 = {
+    email: "sabinhamal@gmail.com",
+    age: 19
+}
+
+let obj2 = obj1;
+obj1.email = "123sabin@gmail.com";
+
+console.log(obj1.email); //123sabin@gmail.com
+console.log(obj2.email); //123sabin@gmail.com
+
+//Both get the reference of the changed value
